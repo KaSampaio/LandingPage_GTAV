f94745e660fb4fc961069bc0894573b71cb15826
Kaio Sampaio de Souza <135068978+KaSampaio@users.noreply.github.com>
2024-04-05 05:12:44 -0300

    Update index.js
    Alteração da linha 23 de "toggle" para "add"

--- src/js/index.js
+++ src/js/index.js
@@ -20,5 +20,5 @@ const botao = document.querySelector(".btn-plataforma");
 const elementoPlataformas = document.querySelector(".btn-plataforma .plataformas");
 
 botao.addEventListener("click", () => {
-    elementoPlataformas.classList.toggle("ativo");
+    elementoPlataformas.classList.add("ativo");
 });
